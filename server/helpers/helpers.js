cdc137c87afed69ad52f17d202f3becf667cbff6
Cruzinshtern <bohdanmatv1@gmail.com>
2021-03-25 19:59:01 +0200

    minor refactorings

---
+++ server/helpers/helpers.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+
+class Helpers {
+    static paginate(req) {
+        const currentPage = parseInt(req.query.page) || process.env.DEFAULT_PAGE;
+        const itemsPerPage = parseInt(req.query.limit) || process.env.DEFAULT_LIMIT;
+        const offset = (currentPage - 1) * itemsPerPage;
+        return { currentPage, itemsPerPage, offset }
+    }
+
+    static async hashed(password) {
+        return await bcrypt.hash(password, 10)
+    }
+}
+
+module.exports = Helpers;
--- server/user/service.js
+++ server/user/service.js
@@ -1,13 +1,9 @@
 const User = require('../models/user-schema')
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const helpers = require('../helpers/helpers.js')
 require("dotenv/config");
 
 class UserService {
-
-    async hashed(password) {
-        return await bcrypt.hash(password, 10)
-    }
 
     async createUser(req) {
         try {
@@ -15,7 +11,8 @@ class UserService {
             if(isMatch) {
                 return { message: 'This email already exists' }
             }
-            const hashedPassword = await this.hashed(req.body.password);
+            // const hashedPassword = await this.hashed(req.body.password);
+            const hashedPassword = await helpers.hashed(req.body.password)
             const newUser = new User({
                 name: req.body.name,
                 username: req.body.username,
@@ -31,17 +28,13 @@ class UserService {
     async findAllUsers(req) {
         try {
             const length = await User.find().countDocuments();
-            const currentPage = parseInt(req.query.page) || process.env.DEFAULT_PAGE;
-            const itemsPerPage = parseInt(req.query.limit) || process.env.DEFAULT_LIMIT;
-            const offset = (currentPage - 1) * itemsPerPage;
-
-            const data = await User.find().limit(itemsPerPage).skip(offset);
-            console.log(data);
+            const paginate = helpers.paginate(req)
+            const data = await User.find().limit(paginate.itemsPerPage).skip(paginate.offset);
             if(data) {
                 const response = {
                     items: data.length ? data : [],
-                    currentPage: currentPage,
-                    itemsPerPage: itemsPerPage,
+                    currentPage: paginate.currentPage,
+                    itemsPerPage: paginate.itemsPerPage,
                     totalItems: length,
                 };
                 return response;
@@ -97,8 +90,5 @@ class UserService {
         );
         return {token};
     }
-
-
-
 }
 module.exports = new UserService;
