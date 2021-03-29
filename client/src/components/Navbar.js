f8d17aaca1c02aa5e06ad6ce49193361fe889054
Cruzinshtern <bohdanmatv1@gmail.com>
2021-03-29 17:18:21 +0300

    front-minor changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,19 @@
 import {Link} from "react-router-dom";
 
 
-function Navbar () {
+function Navbar() {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/new-post'>New post</Link>
-                    </li>
-                    <li>
-                        <Link to='/users'>All users</Link>
-                    </li>
-                    <li>
-                        <Link to='/posts'>All posts</Link>
-                    </li>
-                    <li>
-                        <Link to='/test'>My posts</Link>
-                    </li>
+        <nav>
+            <div className="nav-wrapper">
+                <li className="left brand-logo"><Link to="/homepage">MyPost</Link></li>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><Link to="/new-post">New Post</Link></li>
+                    <li><Link to="/users">All users</Link></li>
+                    <li><Link to="/posts">All posts</Link></li>
+                    <li><Link to="/test">My posts</Link></li>
                 </ul>
-            </nav>
-        </div>
+            </div>
+        </nav>
     )
 }
 
